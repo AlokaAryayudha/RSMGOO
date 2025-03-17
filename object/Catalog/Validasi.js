456e42f4cbf210814531c9f1138e76a1e7e28cb5
AlokaAryayudha <Aaloka221@gmail.com>
2025-03-17 23:01:29 +0700

    Update Validasi

--- object/Catalog/Validasi.js
+++ object/Catalog/Validasi.js
@@ -6,8 +6,15 @@ export class Validasi{
 
         this.iframe = page.frameLocator('iframe[name="iframe"]')
 
-        this.search = page.iframe.getByRole('searchbox', { name: 'Search:' })
-        this.datauser = page.iframe.getByRole('gridcell', { name: '${data}' })
+        this.search = this.iframe.getByRole('searchbox', { name: 'Search:' })
+        this.datauser = this.iframe.getByRole('gridcell', { name: `${data}` })
+        this.personal = this.iframe.getByText('Informasi Personal (Target :')
+        this.pendidikan = this.iframe.getByText('Informasi Pendidikan (Target')
+        this.keluarga = this.iframe.getByText('Informasi Keluarga (Target :')
+        this.ktp = this.iframe.getByText('Informasi KTP (Target : 7)')
+        this.domisili = this.iframe.getByText('Informasi Domisili (Target :')
+        this.kembali = this.iframe.locator('button').filter({ hasText: 'Kembali' })
+
 
     }
 }
