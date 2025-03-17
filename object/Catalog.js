cdd7e0e27ec31dfb012418243bb931123ef375db
AlokaAryayudha <Aaloka221@gmail.com>
2025-03-17 22:32:13 +0700

    Update Catalog

--- object/Catalog.js
+++ object/Catalog.js
@@ -15,4 +15,39 @@ export class MenuCatalog{
         this.MenuExperience = page.getByRole('link', { name: 'My Experience' })
 
     }
+    Kandidat = async() => {
+        console.log("Masuk menu kandidat")
+        await this.MenuKandidat.waitFor()
+        await this.MenuKandidat.click()
+    }
+    Perusahaan = async() => {
+        console.log("Masuk menu perusahaan")
+        await this.MenuPerusahaan.waitFor()
+        await this.MenuPerusahaan.click()
+    }
+    Sekolah = async() => {
+        console.log("Masuk menu Sekolah")
+        await this.MenuSekolah.waitFor()
+        await this.MenuSekolah.click()
+    }
+    Aktivasi = async() => {
+        console.log("Masuk menu Aktivasi")
+        await this.MenuAktivasi.waitFor()
+        await this.MenuAktivasi.click()
+    }
+    Validasi = async() => {
+        console.log("Masuk menu Validasi")
+        await this.MenuValidasi.waitFor()
+        await this.MenuValidasi.click()
+    }
+    Skill = async() => {
+        console.log("Masuk menu Skill")
+        await this.MenuSkill.waitFor()
+        await this.MenuSkill.click()
+    }
+    Experience = async() => {
+        console.log("Masuk menu Experience")
+        await this.MenuExperience.waitFor()
+        await this.MenuExperience.click()
+    }
 }
